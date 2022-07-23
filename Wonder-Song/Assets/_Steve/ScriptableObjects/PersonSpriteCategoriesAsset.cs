9d45abe5b620e6500f3f10434e44b3d0417481bd
Steve Mcilwain <16359890+stevemcilwain@users.noreply.github.com>
2022-07-23 16:02:25 -0400

    PersonGenerator, Person, SpriteChooser all working

---
+++ Wonder-Song/Assets/_Steve/ScriptableObjects/PersonSpriteCategoriesAsset.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PersonSpriteCategoriesAsset", menuName = "People/PersonSpriteCategoriesAsset")]
+public class PersonSpriteCategoriesAsset : ScriptableObject
+{
+    public string HAIR_CATEGORY;
+    public string BANGS_CATEGORY;
+    public string FACE_CATEGORY;
+    public string SHIRT_CATEGORY;
+}
