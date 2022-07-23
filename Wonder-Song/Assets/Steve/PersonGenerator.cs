c5dbb55da9749bc61257bd1ac7b67174a08c4414
Steve Mcilwain <16359890+stevemcilwain@users.noreply.github.com>
2022-07-23 11:30:35 -0400

    Added Steve folder, sprites and building person generation...

---
+++ Wonder-Song/Assets/Steve/PersonGenerator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonGenerator : MonoBehaviour
+{
+
+    [Space(10)]
+    [Header("References")]
+    [Space(10)]
+
+
+    [SerializeField] private GameObject prefab;
+
+    [Space(10)]
+    [Header("Settings")]
+    [Space(10)]
+
+    [SerializeField] private Vector2 offscreen;
+
+    public void Generate(Vector2 position, Transform parent)
+    {
+
+        GameObject go = Instantiate(prefab, offscreen, Quaternion.identity, parent);
+
+        // choose random sprites
+
+        // set the position to make it "live"
+
+    }
+}
